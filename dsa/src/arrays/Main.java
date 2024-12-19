d70c6b1a8c1782d3cd318e8978d6cd458f19a248
hitesh <mrhiteshchandra1998@gmail.com>
2024-12-19 17:29:08 +0530

    2 pointer- foursum

--- dsa/src/arrays/Main.java
+++ dsa/src/arrays/Main.java
@@ -1,6 +1,9 @@
 package arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
@@ -48,5 +51,40 @@ public class Main {
         return res;
     }
     
-    
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums == null || nums.length < 4) return ans;
+
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates
+            for (int j = i + 1; j < n; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // Skip duplicates
+
+                int l = j + 1, r = n - 1;
+                while (l < r) {
+                    long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[l];
+                    sum += nums[r];
+                    if (sum == target) {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
+                        l++;
+                        r--;
+                        
+                        // Skip duplicates
+                        while (l < r && nums[l] == nums[l - 1]) l++;
+                        while (l < r && nums[r] == nums[r + 1]) r--;
+                    } else if (sum < target) {
+                        l++;
+                    } else {
+                        r--;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
 }
