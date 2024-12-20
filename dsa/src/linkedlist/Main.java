158a7ef53a5f55d053a122053bd17fecbf5bc531
hitesh <mrhiteshchandra1998@gmail.com>
2024-12-20 17:33:05 +0530

    find middle in linkedlist

--- dsa/src/linkedlist/Main.java
+++ dsa/src/linkedlist/Main.java
@@ -9,16 +9,29 @@ package linkedlist;
   }
 
 public class Main {
-	 public ListNode reverseList(ListNode head) {
-	        ListNode cur=head,prev=null,next=null;
+	 
+	public ListNode reverseList(ListNode head) {
+        ListNode cur=head,prev=null,next=null;
 
-	        while(cur!=null){	
-	            next=cur.next;
-	            cur.next=prev;
-	            prev=cur;
-	            cur=next;
-	        }
-	        return prev;
-	        
-	    }
+        while(cur!=null){	
+            next=cur.next;
+            cur.next=prev;
+            prev=cur;
+            cur=next;
+        }
+        return prev;
+        
+    }
+    public ListNode middleNode(ListNode head) {
+        
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+
+        return slow;
+    }
 }
