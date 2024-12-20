f6e2b056a6970fbdf95f5406ef7f683985dd6e24
hitesh <mrhiteshchandra1998@gmail.com>
2024-12-20 17:53:58 +0530

    merge two linked list

--- dsa/src/linkedlist/Main.java
+++ dsa/src/linkedlist/Main.java
@@ -34,4 +34,28 @@ public class Main {
 
         return slow;
     }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode temp = new ListNode();
+        ListNode cur = temp;
+
+        while(list1!=null&&list2!=null){
+            if(list1.val<list2.val){
+                cur.next=list1;
+                list1=list1.next;
+            }else{
+               cur.next=list2;
+                list2=list2.next;
+            }
+            cur=cur.next;
+        }
+
+        if(list1==null){
+           cur.next=list2;
+        }
+         if(list2==null){
+           cur.next=list1;
+        }
+
+        return temp.next;
+    }
 }
